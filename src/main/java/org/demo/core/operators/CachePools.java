45889ec4b292274ae28e4b015cf26f8cbfeb4430
Knouton <alex95kokotov@gmail.com>
2022-11-19 12:28:54 +0500

    Добавлен пул стрингов

--- src/main/java/org/demo/core/operators/CachePools.java
+++ src/main/java/org/demo/core/operators/CachePools.java
@@ -5,9 +5,8 @@ public class CachePools {
 		//пул стрингов
 		String stringA = "test";
 		String stringB = "test";
-		System.out.println("Выдаст true если сделать строку " +
-				                   "через просто '=', т.е. бдут ссылаться на одну " +
-				                   "и ту же строку в памяти: "+(stringA == stringB));
+		//Выдаст true если сделать строку через просто '=', т.е. бдут ссылаться на одну и ту же строку в памяти
+		System.out.println("Выдаст true если сделать строку через просто '=': " +(stringA == stringB));
 
 		String stringC = new String("test");
 		String stringD = new String("test");
