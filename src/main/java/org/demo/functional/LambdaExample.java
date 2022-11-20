1af3dc90a2ed3edd2b7c3107c1e5b717c7b47d6f
Knouton <alex95kokotov@gmail.com>
2022-11-20 22:51:15 +0500

    4.5.9	Java core. Интерфейсы, функциональное программирование	Что такое лямбда выражение? зачем они нужны?

---
+++ src/main/java/org/demo/functional/LambdaExample.java
@@ -0,0 +1,17 @@
+package org.demo.functional;
+
+public class LambdaExample {
+
+		public static void main(String[] args) {
+
+			Operationable<Integer> operation1 = (x, y)-> x + y;
+			Operationable<String> operation2 = (x, y) -> x + y;
+
+			System.out.println(operation1.calculate(20, 10)); //30
+			System.out.println(operation2.calculate("20", "10")); //2010
+		}
+	}
+	interface Operationable<T>{
+		T calculate(T x, T y);
+
+}
