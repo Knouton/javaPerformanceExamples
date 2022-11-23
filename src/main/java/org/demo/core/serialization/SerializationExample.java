72e728e7aaed36e01faf56843b41d1bb88a1b5dd
Knouton <alex95kokotov@gmail.com>
2022-11-23 16:37:56 +0500

    4.15.3	Java core. Сериализация и десериализация объектов	Зачем нужно поле serialVersionUID в классе?

--- src/main/java/org/demo/core/serialization/SerializationExample.java
+++ src/main/java/org/demo/core/serialization/SerializationExample.java
@@ -8,44 +8,69 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 public class SerializationExample {
+
+		public static void main(String[] args) throws IOException, ClassNotFoundException {
+			Person person = new Person();
+			person.setAge(20);
+			person.setName("Joe");
+			person.setHeight(10);
+
+			System.out.println("Before: \n" + person);
+
+			String filePath = "D:\\SerializationExample.txt";
+
+			FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+
+			try (ObjectOutputStream objectOutputStream
+					     = new ObjectOutputStream(fileOutputStream)) {
+				objectOutputStream.writeObject(person);
+				objectOutputStream.flush();
+			}
+			FileInputStream fileInputStream = new FileInputStream(filePath);
+
+			try (ObjectInputStream objectInputStream
+					     = new ObjectInputStream(fileInputStream);) {
+				Person p2 = (Person) objectInputStream.readObject();
+				System.out.println("After: \n" + p2);
+			}
+		}
+
+	public static class Person implements Serializable {
+			//Когда мы не объявляем SerialVersionUID в нашем классе,
+			//Java делает это за нас, но этот процесс чувствителен ко многим метаданным класса
+			//рекомендуется определять это поле самому, чтобы не получить неожиданный InvalidClassException при десериализация
+		private static final long serialVersionUID = 1L;
+		static String country = "ITALY";
+		private int age;
 		private String name;
-		private String description;
+		transient int height;
+		public void setAge(final int age) {
+			this.age = age;
+		}
 
-		public SerializationExample(String name, String description) {
+		public String getName() {
+			return name;
+		}
+
+		public void setName(final String name) {
 			this.name = name;
-			this.description = description;
 		}
 
-
-		private void writeObject(ObjectOutputStream stream) throws IOException {
-			stream.defaultWriteObject();
-			System.out.println("Our writeObject");
-		}
-
-		private void readObject(ObjectInputStream stream) throws IOException, ClassNotFoundException {
-			stream.defaultReadObject();
-			System.out.println("Our readObject");
+		public void setHeight(final int height) {
+			this.height = height;
 		}
 
 		@Override
 		public String toString() {
-			return "Talda{" +
-					"name='" + name + '\'' +
-					", description='" + description + '\'' +
+			return "Person{" +
+					" serialVersionUID=" + serialVersionUID +
+					", country=" + country +
+					", age=" + age +
+					", name='" + name + '\'' +
+					", height=" + height +
 					'}';
 		}
 
-		public static void main(String[] args) throws IOException, ClassNotFoundException {
-			SerializationExample partizanka = new SerializationExample("Partizanka", "Viiiski");
-			System.out.println("Before: \n" + partizanka);
+	}
 
-			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("SerializationExample.out"));
-			out.writeObject(partizanka);
-			out.close();
-
-			ObjectInputStream in = new ObjectInputStream(new FileInputStream("SerializationExample.out"));
-			partizanka = (SerializationExample) in.readObject();
-
-			System.out.println("After: \n" + partizanka);
-		}
 }
